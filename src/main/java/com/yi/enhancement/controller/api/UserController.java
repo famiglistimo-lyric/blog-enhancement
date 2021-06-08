cbee320b83236bbf03f57b6c761d0d7f7f00b967
刘文鉴 <lyric0926@163.com>
2021-06-08 20:47:43 +0800

    基础画面

--- src/main/java/com/yi/enhancement/controller/api/UserController.java
+++ src/main/java/com/yi/enhancement/controller/api/UserController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.apache.logging.log4j.message.MapMessage.MapFormat.JSON;
+
 /**
  * <p>
  * 用户表	 前端控制器
